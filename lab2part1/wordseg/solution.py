7286f95d095b961fa4d481b10a8679d55aed596c
qsliu <qsliu2017@outlook.com>
2021-11-11 14:19:17 +0800

    init lab2part1

---
+++ lab2part1/wordseg/solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def crf_predict(self, sentences: List[str]) -> List[str]:
+        results = []
+        # TODO
+        return results
+
+    def dnn_predict(self, sentences: List[str]) -> List[str]:
+        results = []
+        # TODO
+        return results
